7d8b105a622f0da8c72621f65bcb4d0be28dfecb
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2021-03-30 01:16:04 -0400

    Configure DB, eu-US language

---
+++ BarbieFashion/Data/BarbieFashionContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarbieFashion.Data
+{
+    public class BarbieFashionContext : DbContext
+    {
+        public BarbieFashionContext (DbContextOptions<BarbieFashionContext> options)
+            : base(options)
+        {
+        }
+    }
+}
